e254f4408061668d0f0ae2d1e09bcd36198c831d
Takuma Seimiya <mj.soccer.t.s0802@gmail.com>
2019-01-26 18:32:53 +0900

    PlayerMove機能

---
+++ MofuttoRoyale/Assets/Scritps/Player/BasePlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BasePlayer : MonoBehaviour
+{
+    private int _playerID;
+    public int PlayerID { get { return _playerID; } }
+
+    private IInputProvider _inputProvider;
+    protected IInputProvider InputProvider { get { return _inputProvider; } }
+
+    void Awake()
+    {
+        _inputProvider = GetComponent<IInputProvider>();
+    }
+
+    public void SetPlayer(int id)
+    {
+        _playerID = id;
+        OnStart();
+        Initialize();
+    }
+
+
+    protected abstract void OnStart();
+    protected abstract void Initialize();
+}
---
+++ MofuttoRoyale/Assets/Scritps/Player/PlayerMover.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class PlayerMover : BasePlayer
+{
+    public float moveSpeed = 5;
+    public bool isAttacking = false;
+
+    private Rigidbody _rigidbody;
+
+    public void Set()
+    {
+        Initialize();
+    }
+
+    protected override void OnStart()
+    {
+        
+    }
+
+    protected override void Initialize()
+    {
+        Debug.Log("Initialize,Mover");
+        _rigidbody = GetComponent<Rigidbody>();
+        InputProvider.MoveDirection
+            .Subscribe(x => 
+            {
+                var value = x.normalized * moveSpeed;
+                Debug.Log("Move:" + value);
+                Move(value);
+            });
+        
+        InputProvider.AttackButton
+            .Where(x => x && !isAttacking)
+            .Subscribe(x => 
+            {
+                Attack();
+            });
+    }
+
+    private void Move(Vector3 moveDirection)
+    {
+        _rigidbody.AddForce(moveDirection, ForceMode.Acceleration);
+    }
+
+    private void Attack()
+    {
+        Debug.Log("Attack!");
+        isAttacking = true;
+        
+    }
+}
