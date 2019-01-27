37eb7aa7897bdd05bac7771d165e263c53e6ea39
Takuma Seimiya <mj.soccer.t.s0802@gmail.com>
2019-01-27 13:57:13 +0900

    タイトルシーン変更

---
+++ MofuttoRoyale/Assets/Scritps/FlashingOBJECT.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class FlashingOBJECT : MonoBehaviour
+{
+    [SerializeField] private float angularFrequency = 5f;
+    private Image image;
+
+    void Start()
+    {
+        float time = 0.0f;
+        image = GetComponent<Image>();
+        Observable.Interval(TimeSpan.FromSeconds(Time.deltaTime)).Subscribe(_ =>
+          {
+              time += angularFrequency * Time.deltaTime;
+              var color = image.color;
+              color.a = Mathf.Sin(time) * 0.5f + 0.5f;
+              image.color = color;
+          }).AddTo(this);
+    }
+}
