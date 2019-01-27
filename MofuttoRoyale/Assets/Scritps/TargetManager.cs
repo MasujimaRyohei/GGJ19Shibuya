caa642e969b4e63463b4b755b5d1b8cfd800b01d
MasujimaRyohei <info@masujimaryohei.jp>
2019-01-27 15:45:54 +0900

    スコアにUIが対応するようにした

--- MofuttoRoyale/Assets/Scritps/TargetManager.cs
+++ MofuttoRoyale/Assets/Scritps/TargetManager.cs
@@ -12,25 +12,41 @@ public class TargetManager : MonoBehaviour
         set { target = value; }
     }
 
-    [SerializeField] private GameObject hint1Popup;
-    [SerializeField] private GameObject hint2Popup;
+    [SerializeField] private Image hint1Popup;
+    [SerializeField] private Image hint2Popup;
 
     [SerializeField] private Sprite roofGreen;
-    [SerializeField] private Image roofRed;
-    [SerializeField] private Image roofBlue;
-    [SerializeField] private Image roofYellow;
-    [SerializeField] private Image window;
-    [SerializeField] private Image chimney;
+    [SerializeField] private Sprite roofRed;
+    [SerializeField] private Sprite roofBlue;
+    [SerializeField] private Sprite roofYellow;
+    [SerializeField] private Sprite window;
+    [SerializeField] private Sprite chimney;
 
     // Start is called before the first frame update
     private void Start()
     {
-        Instantiate(roofGreen, hint1Popup.transform);
+     
     }
 
-    // Update is called once per frame
-    void Update()
+public void ShowHint()
     {
+        hint1Popup.gameObject.SetActive(true);
+
+        switch (target.RoofColor)
+        {
+            case ERoofColor.Red:
+                hint1Popup.sprite = roofRed;
+                break;
+            case ERoofColor.Yellow:
+                hint1Popup.sprite = roofYellow;
+                break;
+            case ERoofColor.Green:
+                hint1Popup.sprite = roofGreen;
+                break;
+            case ERoofColor.Blue:
+                hint1Popup.sprite = roofBlue;
+                break;
+        }
 
     }
 }
